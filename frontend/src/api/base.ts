5987f9c28913a09f57aee50cb21ac2b9579947e3
Mohamed981 <mohamedakrama153@gmail.com>
2023-02-08 04:02:21 +0200

    original

--- frontend/src/api/base.ts
+++ frontend/src/api/base.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const apiClient = axios.create({
-  baseURL: "https://shop-server-voaa.onrender.com/api", // <- ENV variable
+  baseURL: "http://localhost:5000/api", // <- ENV variable
 });
 apiClient.interceptors.request.use((config) => {
   return ({
